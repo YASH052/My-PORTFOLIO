bcc77341a5aa6e1da39782619f063045708080a2
YASH052 <yashsharma21998@gmail.com>
2024-02-19 21:15:06 +0530

    commit2

---
+++ client/src/pages/About/About.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './About.css'
+const About = () => {
+    return (
+        <>
+            <div className='about'>
+                <div className="row">
+                    <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-img">
+                        <img src="/photo.jpg" alt="Profile_Pic" />
+                    </div>
+                    <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
+                        <h1>About Me!</h1>
+                        <p>Lorem ipsum dolor sit amet
+                            consectetur adipisicing elit.
+                            Doloribus maxime modi sapiente, beatae, magnam aut dicta harum
+                            quae hic quis corporis eveniet
+                            ea cumque laborum dolores qui illo vero incidunt!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            A voluptatibus distinctio deserunt eos nobis ad eveniet amet?
+                            Itaque fugiat debitis quaerat, distinctio labore accusantium,
+                            deserunt eum est dolores voluptas quod? Lorem ipsum dolor sit amet
+                            consectetur adipisicing elit.
+                            Doloribus maxime modi sapiente, beatae, magnam aut dicta harum
+                            quae hic quis corporis eveniet
+                            ea cumque laborum dolores qui illo vero incidunt!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            A voluptatibus distinctio deserunt eos nobis ad eveniet amet?
+                            Itaque fugiat debitis quaerat, distinctio labore accusantium,
+                            deserunt eum est dolores voluptas quod?</p>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default About
